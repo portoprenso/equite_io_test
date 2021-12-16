043ba71ac474d783ba065093fc0f60902632e532
portoprenso <portoprenso@gmail.com>
2021-12-16 20:17:03 +0600

    fixing header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,19 +1,61 @@
 import React from 'react';
 import mainLogo from '../../assets/logo_main.svg'
 import Image from 'next/image';
+// import { Box, positions } from '@mui/system';
+import {Typography, List, ListItem, Toolbar, Button, Grid} from '@mui/material';
 
 
 function Header() {
   return (
-    <nav>
-      <Image src={mainLogo} />
-      <ul>
-        <li>Главная</li>
-        <li>Рейтинги</li>
-        <li>Блог</li>
-        <li>PRO</li>
-      </ul>
-    </nav>
+    <Grid
+      item
+      xs={12}
+      sx={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+      }}>
+      <Grid
+        item
+        xs={7}
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}
+      >
+        <Image src={mainLogo} alt={"Main Logo"}/>
+        <Link></Link>
+        <Typography variant='subtitle1' sx={{color: 'white', zIndex: 0}}>ГЛАВНАЯ</Typography>
+        <Typography variant='subtitle1' sx={{color: 'white', zIndex: 0}}>РЕЙТИНГИ</Typography>
+        <Typography variant='subtitle1' sx={{color: 'white', zIndex: 0}}>БЛОГ</Typography>
+        <Typography variant='subtitle1' sx={{color: 'white', zIndex: 0}}>PRO</Typography>
+        <Grid>
+          <Button
+            sx={{
+              borderRadius: 1.25,
+              border: '1px solid white',
+              color: 'white',
+            }}>Инвестору</Button>
+        </Grid>
+      </Grid>
+      <Grid
+        item
+        xs={3}
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}
+      >
+        <Grid item xs={"auto"} sx={{backgroundColor: '#101C2B', borderRadius: 1.25}}><Button
+          sx={{color: '#AE8DE5'}}>RU</Button></Grid>
+        <Grid sx={{backgroundColor: '#101C2B', border: '1px solid #AE8DE5', borderRadius: 1.25,}}><Button
+          sx={{color: '#AE8DE5'}}>Войти</Button></Grid>
+        <Grid sx={{backgroundColor: '#5040B2', borderRadius: 1.25,}}><Button
+          sx={{color: 'white'}}>Начать</Button></Grid>
+      </Grid>
+    </Grid>
   );
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,15 @@
-import { createTheme, ThemeProvider } from '@mui/system';
-import type { NextPage } from 'next'
+import {Container, createTheme, Grid, ThemeProvider} from '@mui/material';
+import type {NextPage} from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+// import styles from '../styles/Home.module.css'
 import Header from './../components/Header/Header';
+import simpleBackground from '../assets/simplified_background.png'
 
 const theme = createTheme({
   palette: {
     background: {
-      paper: '#fff',
+      paper: '#101C2B',
     },
     text: {
       primary: '#173A5E',
@@ -18,77 +19,51 @@ const theme = createTheme({
       active: '#001E3C',
     },
     success: {
-      dark: '#009688',
+      main: '#009688'
     },
   },
 });
 
-
 const Home: NextPage = () => {
   return (
     <ThemeProvider theme={theme}>
-    <div className={styles.container}>
       <Head>
         <title>EQUITE.IO</title>
-        <meta name="description" content="Equite.io" />
-        <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="Equite.io"/>
+        <link rel="icon" href="/favicon.ico"/>
       </Head>
-
-      <main className={styles.main}>
-        <Header />
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Grid
+        sx={{
+          backgroundColor: 'background.paper',
+          justifyContent: 'center',
+          minHeight: '100vh',
+          position: 'relative',
+        }}
+      >
+        <div
+          className={'simplebackground'}
         >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
-    </div>
+          <Image
+            src={simpleBackground}
+          />
+        </div>
+        <Grid
+          container
+          sx={{
+            justifyContent: 'center',
+            maxWidth: '1278px',
+            mx: 'auto'
+          }}>
+          <Header/>
+
+        </Grid>
+      </Grid>
+      <style>{`
+          .simplebackground {
+              position:absolute;
+          }
+      `}</style>
+
     </ThemeProvider>
   )
 }
